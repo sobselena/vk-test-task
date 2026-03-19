512099efe04a3ec5e456b25e6a0d00567b97d3db
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-03-19 20:27:48 +0300

    feat: add basic normalize styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import { ErrorBoundary } from './components/error-boundary';
+import './app/styles/index.css';
+const root = document.querySelector('#root');
 
-createRoot(document.querySelector('#root')!).render(
+if (!root) throw new Error('Root container not found');
+
+createRoot(root).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
